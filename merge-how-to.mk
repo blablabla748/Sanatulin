200b625a828164722e5a194db03c90568d909332
blablabla748 <aleks.klub12@gmail.com>
2025-04-28 22:56:40 +0300

    merge instruction

---
+++ merge-how-to.mk
@@ -0,0 +1,4 @@
+## Слияние веток на GitHub (Pull Request)
+
+1.  Создайте Pull Request (PR) из вашей ветки в целевую (обычно `main`).
+2.  После одобрения PR, нажмите "Merge pull request" и "Confirm merge".
